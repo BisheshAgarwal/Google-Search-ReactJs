842160611a3ce35c6dd3bd1cbc466ff4b78443c0
Bishesh Agarwal <bisheshagarwal1999@gmailcom>
2022-07-21 13:44:06 +0530

    Setup react router and design the basic footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Routes from "./components/Routes";
+import Router from "./components/Router";
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200">App</div>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Router />
+        <Footer />
+      </div>
     </div>
   );
 };
